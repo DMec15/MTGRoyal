7450aeac334cfab9bc80d4d1a3fc022c32b834cb
irvingomarguerrero-creator <irvingomarguerrero@gmail.com>
2026-05-20 03:53:38 -0600

    Buscador de Home y vista de Detalles conectado a BD

--- Controllers/CartasController.cs
+++ Controllers/CartasController.cs
@@ -1,14 +1,28 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MTGRoyal.Models;
 
 namespace MTGRoyal.Controllers
 {
     public class CartasController : Controller
     {
-        public IActionResult Detalle(string id)
-        {
-            ViewBag.CardId = id;
+        private readonly MtgroyalDbContext _db;
 
-            return View();
+        public CartasController(MtgroyalDbContext db)
+        {
+            _db = db;
+        }
+
+        public async Task<IActionResult> Detalle(int id)
+        {
+            var carta = await _db.Cartas
+                .Include(c => c.Colors)
+                .FirstOrDefaultAsync(c => c.Id == id);
+
+            if (carta == null)
+                return NotFound();
+
+            return View(carta);
         }
     }
 }
